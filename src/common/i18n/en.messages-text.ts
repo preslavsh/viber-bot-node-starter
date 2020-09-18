5c190e7c6befb747e407f35dee451e838dba12fa
preslavsh <preskosemov@abv.bg>
2020-09-18 15:38:13 +0300

    add default messages

--- src/common/i18n/en.messages-text.ts
+++ src/common/i18n/en.messages-text.ts
@@ -1,10 +1,10 @@
 export const enMessageText = {
-    GREETINGS_FIRST_TIME: ``,
-    GREETINGS_AGAIN: ``,
-    GREETINGS_KEYBOARD_BUTTON: ``,
-    SETUP: ``,
-    END_SETUP: ``,
-    MAIN_KEYBOARD_SHARE: ``,
-    SHARE: ``,
-    RANDOM: ``,
+    GREETINGS_FIRST_TIME: `Hello`,
+    GREETINGS_AGAIN: `Hello`,
+    GREETINGS_KEYBOARD_BUTTON: `Start`,
+    SETUP: `Setup`,
+    END_SETUP: `End Setup`,
+    MAIN_KEYBOARD_SHARE: `Share`,
+    SHARE: `You can share with Friends in Viber`,
+    RANDOM: `Sorry I didn't get that`,
 };
